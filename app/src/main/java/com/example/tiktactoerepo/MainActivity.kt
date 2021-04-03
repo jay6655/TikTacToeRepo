c214ef4af833e2a0aab1d81f8fdf75fe4368aea5
jay6655 <mistry.jay6655@gmail.com>
2021-04-03 19:31:41 +0530

    add file

--- app/src/main/java/com/example/tiktactoerepo/MainActivity.kt
+++ app/src/main/java/com/example/tiktactoerepo/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.tiktactoerepo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val singlePlayer =  findViewById<Button>(R.id.singlePlayer);
+        singlePlayer.setOnClickListener {
+            Log.e("AAA", "singlePlayer Clicked ");
+
+            val intent = Intent(this@MainActivity,PlayingActivity::class.java);
+            startActivity(intent)
+        }
+
+        val multiplePlayer =  findViewById<Button>(R.id.multiplePlayer);
+        multiplePlayer.setOnClickListener {
+            Log.e("AAA", "multiplePlayer Clicked ");
+        }
     }
 }
